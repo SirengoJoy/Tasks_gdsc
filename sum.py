9769f363fcd8492193b0ee47a8499b564ce89924
SirengoJoy <148388998+SirengoJoy@users.noreply.github.com>
2024-05-13 22:18:52 +0300

    first commit

---
+++ sum.py
@@ -0,0 +1,10 @@
+#a program that runs the summation of two numbers requested by the user.
+
+
+number1 = float(input("Please enter your first number: "))
+
+number2 = float(input("Please enter your second number: "))
+
+sum = number1 + number2
+
+print("Sum", sum)
